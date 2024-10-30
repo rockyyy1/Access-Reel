3a1260e993b1d28d4422f0a536a0671232f77992
rockyyy1 <124854700+rockyyy1@users.noreply.github.com>
2024-10-30 17:40:54 +0800

    debugged and retreived each wrapped
    Don't know how to get Top User Rated Reviews

--- AccessReel/AccessReel/MainPage.xaml.cs
+++ AccessReel/AccessReel/MainPage.xaml.cs
@@ -18,13 +18,11 @@ namespace AccessReel
             Retrieve(ReadWebsite());
         }
 
+        // Returns the html
         private string ReadWebsite()
         {
-            
             var web = new HtmlWeb();
-            text = web.Load("https://accessreel.com/").Text;
-            //Debug.WriteLine(text);
-            
+            text = web.Load("https://accessreel.com/").Text;            
             return text;
         }
 
@@ -34,10 +32,12 @@ namespace AccessReel
             document.LoadHtml(text);
 
             var wpd_wrapper = document.DocumentNode.SelectSingleNode("/html/body/div[3]/div[2]/div[3]/div[1]/div[2]/div[1]/div/div");
-            //retrieve list of blog posts
-            HtmlNodeCollection itemsNode = wpd_wrapper.SelectNodes("//section[contains(@class, 'gp-post-item') and contains(@class, 'type-article')]");
 
-            foreach ( var node in itemsNode )
+            // Retrieve "Latest News"
+            Debug.WriteLine("LASTEST NEWS:");
+            HtmlNodeCollection latestNewsNodes = wpd_wrapper.SelectNodes("//section[contains(@class, 'gp-post-item') and contains(@class, 'type-article') and contains(@class, 'categories-news')]");
+
+            foreach ( var node in latestNewsNodes)
             {
                 var item = node as HtmlNode;
                 string title = item.SelectSingleNode(".//h2[contains(@class, 'gp-loop-title')]//a").GetAttributeValue("title", string.Empty);
@@ -46,8 +46,58 @@ namespace AccessReel
                 HtmlNode? paragraphItem = item.SelectSingleNode(".//div[contains(@class, 'gp-loop-text')]//p");
                 string paragraph = paragraphItem == null ? "" : paragraphItem.InnerText;
 
-                Debug.WriteLine(title + "\n" + paragraph + "\n\n");
+                Debug.WriteLine(title + "\n" + paragraph + "\n" + image + "\n");
             }
+
+            // Retrieve "Latest Interviews"
+            Debug.WriteLine("LASTEST INTERVIEWS:");
+            HtmlNodeCollection latestInterviewsNodes = wpd_wrapper.SelectNodes("//section[contains(@class, 'gp-post-item') and contains(@class, 'type-article') and contains(@class, 'categories-interviews')]");
+
+            foreach (var node in latestInterviewsNodes)
+            {
+                var item = node as HtmlNode;
+                string title = item.SelectSingleNode(".//h2[contains(@class, 'gp-loop-title')]//a").GetAttributeValue("title", string.Empty);
+                string image = item.SelectSingleNode(".//img").GetAttributeValue("src", string.Empty);
+
+                HtmlNode? paragraphItem = item.SelectSingleNode(".//div[contains(@class, 'gp-loop-text')]//p");
+                string paragraph = paragraphItem == null ? "" : paragraphItem.InnerText;
+
+                Debug.WriteLine(title + "\n" + paragraph + "\n" + image + "\n");
+            }
+
+            // Retrieve "Latest Reviews"
+            Debug.WriteLine("LASTEST REVIEWS:");
+            HtmlNodeCollection latestReviewsNodes = wpd_wrapper.SelectNodes("//section[contains(@class, 'gp-post-item') and contains(@class, 'gp_hubs-reviews')]");
+
+            foreach (var node in latestReviewsNodes)
+            {
+                var item = node as HtmlNode;
+                string title = item.SelectSingleNode(".//h2[contains(@class, 'gp-loop-title')]//a").GetAttributeValue("title", string.Empty);
+                string image = item.SelectSingleNode(".//img").GetAttributeValue("src", string.Empty);
+
+                HtmlNode? paragraphItem = item.SelectSingleNode(".//div[contains(@class, 'gp-loop-text')]//p");
+                string paragraph = paragraphItem == null ? "" : paragraphItem.InnerText;
+
+                Debug.WriteLine(title + "\n" + paragraph + "\n" + image + "\n");
+            }
+
+            // Retrieve "Top User Rated Reviews"
+            Debug.WriteLine("LASTEST REVIEWS:" + "\n\n");
+            // Don't really know how to get this...
+
+            //Retrieve "New Trailers"
+            Debug.WriteLine("NEW TRAILERS:");
+            HtmlNodeCollection newTrailersNodes = wpd_wrapper.SelectNodes("//section[contains(@class, 'gp-post-item') and contains(@class, 'type-article') and contains(@class, 'categories-trailers')]");
+
+            foreach (var node in newTrailersNodes)
+            {
+                var item = node as HtmlNode;
+                string title = item.SelectSingleNode(".//h2[contains(@class, 'gp-loop-title')]//a").GetAttributeValue("title", string.Empty);
+                string image = item.SelectSingleNode(".//img").GetAttributeValue("src", string.Empty);
+
+                Debug.WriteLine(title + "\n" + image + "\n\n");
+            }
+
         }
     }
 }
